ac7a63eeda9ca2ec15ca751e2ddb2347e8f2ccc0
jakeviado <jakeviado434@gmail.com>
2025-04-06 20:58:22 +0800

    BREAKING CHANGE: chnages on sample text

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ def plagiarism_checker(text1, text2):
 
 
 text1 = "This is a sample text."
-text2 = "This is another a example text."
+text2 = "This is another a example text that is similar but not identical."
 score = plagiarism_checker(text1, text2)
 print(f"Similarity score: {score}")
